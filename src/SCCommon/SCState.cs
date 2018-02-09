0b4e30660a49f69047ea4696244feb60c98becd4
HarvesteR <vanquish.harvester@gmail.com>
2018-02-09 12:30:27 -0600

    Added button de/serialization to SCState
    gitignore file written for unity projects moved to the actual unity project folder, where it should have been (this is why csproj files weren't committed).

--- src/SCCommon/SCState.cs
+++ src/SCCommon/SCState.cs
@@ -5,76 +5,101 @@ using System.IO;
 public class SCState
 {
 
-    public float _axis0;
-    public float _axis1;
-    public float _axis2;
-    public float _axis3;
-    public float _axis4;
-    public float _axis5;
-    public float _axis6;
-    public float _axis7;
-    public float _axis8;
-    public float _axis9;
-    public float _axis10;
-    public float _axis11;
-    public float _axis12;
-    public float _axis13;
-    public float _axis14;
-    public float _axis15;
+	public float _axis0;
+	public float _axis1;
+	public float _axis2;
+	public float _axis3;
+	public float _axis4;
+	public float _axis5;
+	public float _axis6;
+	public float _axis7;
+	public float _axis8;
+	public float _axis9;
+	public float _axis10;
+	public float _axis11;
+	public float _axis12;
+	public float _axis13;
+	public float _axis14;
+	public float _axis15;
 
-    public bool _buttonHome;
-    public bool _buttonSettings;
-    public bool _buttonRec;
-    public bool _buttonTakeOff;
-    public bool _buttonRTH;
-    public bool _buttonPhoto;
-    public bool _buttonThumbL;
-    public bool _buttonThumbR;
-        
-
-    public SCState()
-    {
-
-    }
+	public bool _buttonHome;
+	public bool _buttonSettings;
+	public bool _buttonRec;
+	public bool _buttonTakeOff;
+	public bool _buttonRTH;
+	public bool _buttonPhoto;
+	public bool _buttonThumbL;
+	public bool _buttonThumbR;
 
 
-    public void Serialize(BinaryWriter bw)
-    {
-        bw.Write(_axis0);
-        bw.Write(_axis1);
-        bw.Write(_axis2);
-        bw.Write(_axis3);
-        bw.Write(_axis4);
-        bw.Write(_axis5);
-        bw.Write(_axis6);
-        bw.Write(_axis7);
-        bw.Write(_axis8);
-        bw.Write(_axis9);
-        bw.Write(_axis10);
-        bw.Write(_axis11);
-        bw.Write(_axis12);
-        bw.Write(_axis13);
-        bw.Write(_axis14);
-        bw.Write(_axis15);
-    }
-    
-    public void Deserialize(byte[] data, int offset = 0)
-    {
-        _axis0 = BitConverter.ToSingle(data, offset);
-        _axis1 = BitConverter.ToSingle(data, 4 + offset);
-        _axis2 = BitConverter.ToSingle(data, 4 * 2 + offset);
-        _axis3 = BitConverter.ToSingle(data, 4 * 3 + offset);
-        _axis4 = BitConverter.ToSingle(data, 4 * 4 + offset);
-        _axis5 = BitConverter.ToSingle(data, 4 * 5 + offset);
-        _axis6 = BitConverter.ToSingle(data, 4 * 6 + offset);
-        _axis7 = BitConverter.ToSingle(data, 4 * 7 + offset);
-        _axis8 = BitConverter.ToSingle(data, 4 * 8 + offset);
-        _axis9 = BitConverter.ToSingle(data, 4 * 9 + offset);
-        _axis10 = BitConverter.ToSingle(data, 4 * 10 + offset);
-        _axis11 = BitConverter.ToSingle(data, 4 * 11 + offset);
-        _axis12 = BitConverter.ToSingle(data, 4 * 12 + offset);
-        _axis13 = BitConverter.ToSingle(data, 4 * 13 + offset);
-        _axis14 = BitConverter.ToSingle(data, 4 * 14 + offset);
-        _axis15 = BitConverter.ToSingle(data, 4 * 15 + offset);
-    }
+	private byte buttons;
+
+	public SCState()
+	{
+
+	}
+
+
+	public void Serialize(BinaryWriter bw)
+	{
+		bw.Write(_axis0);
+		bw.Write(_axis1);
+		bw.Write(_axis2);
+		bw.Write(_axis3);
+		bw.Write(_axis4);
+		bw.Write(_axis5);
+		bw.Write(_axis6);
+		bw.Write(_axis7);
+		bw.Write(_axis8);
+		bw.Write(_axis9);
+		bw.Write(_axis10);
+		bw.Write(_axis11);
+		bw.Write(_axis12);
+		bw.Write(_axis13);
+		bw.Write(_axis14);
+		bw.Write(_axis15);
+
+		// put buttons in a single flag
+		buttons = 0;
+		buttons |= (_buttonHome << 0);
+		buttons |= (_buttonSettings << 1);
+		buttons |= (_buttonRec << 2);
+		buttons |= (_buttonTakeOff << 3);
+		buttons |= (_buttonRTH << 4);
+		buttons |= (_buttonPhoto << 5);
+		buttons |= (_buttonThumbL << 6);
+		buttons |= (_buttonThumbR << 7);
+		br.Write(buttons);
+	}
+
+	public void Deserialize(byte[] data, int offset = 0)
+	{
+		_axis0 = BitConverter.ToSingle(data, offset);
+		_axis1 = BitConverter.ToSingle(data, 4 + offset);
+		_axis2 = BitConverter.ToSingle(data, 4 * 2 + offset);
+		_axis3 = BitConverter.ToSingle(data, 4 * 3 + offset);
+		_axis4 = BitConverter.ToSingle(data, 4 * 4 + offset);
+		_axis5 = BitConverter.ToSingle(data, 4 * 5 + offset);
+		_axis6 = BitConverter.ToSingle(data, 4 * 6 + offset);
+		_axis7 = BitConverter.ToSingle(data, 4 * 7 + offset);
+		_axis8 = BitConverter.ToSingle(data, 4 * 8 + offset);
+		_axis9 = BitConverter.ToSingle(data, 4 * 9 + offset);
+		_axis10 = BitConverter.ToSingle(data, 4 * 10 + offset);
+		_axis11 = BitConverter.ToSingle(data, 4 * 11 + offset);
+		_axis12 = BitConverter.ToSingle(data, 4 * 12 + offset);
+		_axis13 = BitConverter.ToSingle(data, 4 * 13 + offset);
+		_axis14 = BitConverter.ToSingle(data, 4 * 14 + offset);
+		_axis15 = BitConverter.ToSingle(data, 4 * 15 + offset);
+
+		buttons = data[data.Length - 1];
+		_buttonHome = (buttons & 1 << 0) == 1;
+		_buttonSettings = (buttons & 1 << 1) == 1;
+		_buttonRec = (buttons & 1 << 2) == 1;
+		_buttonTakeOff = (buttons & 1 << 3) == 1;
+		_buttonRTH = (buttons & 1 << 4) == 1;
+		_buttonPhoto = (buttons & 1 << 5) == 1;
+		_buttonThumbL = (buttons & 1 << 6) == 1;
+		_buttonThumbR = (buttons & 1 << 7) == 1;
+
+	}
 }
